0698b75ce364fbbed4a8ad73c2fe0f427222d10c
Rajan Mishra <rm347711@gmail.com>
2024-12-18 21:54:02 +0530

    login and signup pages done

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,11 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const CaptainSignup = () => {
-    return (
-        <div>
-            <h1>CaptainSignup page</h1>
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userData, setUserData] = useState({});
 
-export default CaptainSignup
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setUserData({
+      fullName: {
+        firstName: firstName,
+        lastName: lastName,
+      },
+      email: email,
+      password: password,
+    });
+    console.log(userData);
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      <div className="px-5 py-5 h-screen flex flex-col justify-between">
+        <div>
+          <img
+            className="w-16 mb-3"
+            src="https://www.svgrepo.com/show/505031/uber-driver.svg"
+            alt="Uber Logo"
+          />
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+            className="flex flex-col space-y-4"
+          >
+            <h3 className="text-lg w-full font-medium ">Captain's name?</h3>
+            <div className="flex gap-4 mb-6">
+              <input
+                required
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className="bg-[#eeeeee] rounded border px-4 py-2 w-1/2 text-lg placeholder:text-base"
+                type="text"
+                placeholder="First name"
+              />
+
+              <input
+                required
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className="bg-[#eeeeee] rounded border px-4 py-2 w-1/2 text-lg placeholder:text-base"
+                type="text"
+                placeholder="Last name"
+              />
+            </div>
+            <h3 className="text-lg font-medium mb-2">Captain's email?</h3>
+
+            <input
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-[#eeeeee] mb-6 rounded border px-4 py-2 w-full text-lg placeholder:text-base"
+              type="email"
+              placeholder="email@example.com"
+            />
+
+            <h3 className="text-lg font-medium mb-2"> Enter Password</h3>
+
+            <input
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-[#eeeeee] mb-6 rounded border px-4 py-2 w-full text-lg placeholder:text-base"
+              type="password"
+              placeholder="password"
+            />
+
+            <button className="bg-[#111] text-white font-semibold mb-3 rounded  px-4 py-2 w-full">
+              Sign Up
+            </button>
+          </form>
+
+          <p className="text-center">
+            Already have an account?
+            <Link to="/captain-login" className="text-blue-600">
+              Login here
+            </Link>
+          </p>
+        </div>
+
+        <div>
+          <p className="text-[10px] leading-tight">
+            This site is protected by reCAPTCHA and the{" "}
+            <span className="underline">Google's Privacy and Policy</span> and{" "}
+            <span className="underline">Terms of Service apply.</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainSignup;
